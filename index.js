939dc3968c779dcc4c7a16904249d527634e7b01
sarkar niloy <sarkarniloy102@gmail.com>
2024-04-22 00:48:46 +0600

    connect database(mongodb)

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+console.log(process.env.db_USER);
+const uri = `mongodb+srv://${process.env.db_USER}:${process.env.db_PASS}@cluster0.mypgnvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.get('/',(req,res)=>{
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+// Get method route
+app.get('/', (req, res) => {
     res.send('TCG is running');
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`TCG is running on port: ${port}`)
 })
