effe57b6af295c34105cd758e661dd813fbead4b
sarkar niloy <sarkarniloy102@gmail.com>
2024-07-08 03:10:02 +0600

    jwt implement

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -29,6 +30,13 @@ async function run() {
         const popularCollection = client.db("TCG").collection("popular_category");
         const cartCollection = client.db("TCG").collection("carts");
 
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1h' });
+            res.send({ token });
+        })
+
         // post user information
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -37,11 +45,31 @@ async function run() {
             if (emailExist)
                 return res.send({ message: 'user already exist', insertId: null });
             const result = await userCollection.insertOne(user);
+
             res.send(result);
         })
-        // get all users
-        app.get('/users', async (req, res) => {
+        // middleware
+        const verifytoken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbidden access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+
+            // verify token
+            jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, decoded) => {
+                if (err) {
+                    return res.status(401)
+                        .send({ message: 'forbidden access' })
+                }
+                req.decoded = decoded;
+                next();
+            })
+        }
+        // users related api
+        app.get('/users', verifytoken, async (req, res) => {
             const result = await userCollection.find().toArray();
+
             res.send(result);
         })
 
@@ -50,6 +78,19 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        // update role 
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: "admin"
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
             res.send(result);
         })
 
